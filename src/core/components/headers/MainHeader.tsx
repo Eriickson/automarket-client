d294e451d5d0e9bc7ee878f844d5f648e31a3a8a
Erickson Manuel Holguín <38663299+Eriickson@users.noreply.github.com>
2021-06-26 15:49:11 -0400

    Agregando header fijo

--- src/core/components/headers/MainHeader.tsx
+++ src/core/components/headers/MainHeader.tsx
@@ -2,43 +2,46 @@ import React from "react";
 import { Box, Container, Avatar, Button } from "@chakra-ui/react";
 import { signIn, signOut } from "next-auth/client";
 import Link from "next/link";
+import Headroom from "react-headroom";
 
 import { AutomarketRDLogo, AutomarketRDIsotipo } from "../../../assets";
 import { MainMenu } from "./MainMenu";
 
 export const MainHeader = () => {
   return (
-    <Box py={[4, 6]} shadow="sm" borderBottomWidth="1px">
-      <Container display="flex" alignItems="center" justifyContent="space-between">
-        <Link href="/">
-          <Box cursor="pointer" w="56">
-            <AutomarketRDLogo />
-          </Box>
-        </Link>
-        {/* <Box display={[null, "none"]} w="10">
+    <Headroom>
+      <Box py={[4, 6]} shadow="sm" borderBottomWidth="1px" bg="white">
+        <Container display="flex" alignItems="center" justifyContent="space-between">
+          <Link href="/">
+            <Box cursor="pointer" w="56">
+              <AutomarketRDLogo />
+            </Box>
+          </Link>
+          {/* <Box display={[null, "none"]} w="10">
           <AutomarketRDIsotipo />
         </Box> */}
-        <Box display="flex" alignItems="center">
-          <Link href="/login/signup">
-            <Button colorScheme="pri" display={["none", "block"]}>
-              Regístrate
+          <Box display="flex" alignItems="center">
+            <Link href="/login/signup">
+              <Button colorScheme="pri" display={["none", "block"]}>
+                Regístrate
+              </Button>
+            </Link>
+            <Button
+              colorScheme="pri"
+              variant="outline"
+              ml="2.5"
+              display={["none", null, "block"]}
+              onClick={() => signIn()}
+            >
+              Iniciar Sesión
             </Button>
-          </Link>
-          <Button
-            colorScheme="pri"
-            variant="outline"
-            ml="2.5"
-            display={["none", null, "block"]}
-            onClick={() => signIn()}
-          >
-            Iniciar Sesión
-          </Button>
-          <Avatar display="none" shadow="md" name="Kola Tioluwani" src="https://bit.ly/tioluwani-kolawole" />
-          <Box ml="2.5" display={[null, null, "none"]} h="max-content">
-            <MainMenu />
+            <Avatar display="none" shadow="md" name="Kola Tioluwani" src="https://bit.ly/tioluwani-kolawole" />
+            <Box ml="2.5" display={[null, null, "none"]} h="max-content">
+              <MainMenu />
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+    </Headroom>
   );
 };
--- src/core/layouts/LoginLayout.tsx
+++ src/core/layouts/LoginLayout.tsx
@@ -5,28 +5,31 @@ import { MainFooter } from "@/components";
 import { AutomarketRDLogo } from "../../assets";
 import Link from "next/link";
 import { TransitionPageFade } from "@/animations";
+import Headroom from "react-headroom";
 
 export const LoginLayout: FC = ({ children }) => {
   return (
     <TransitionPageFade>
       <Box bgColor="gray.50" minH="100vh" display="flex" flexDirection="column" justifyContent="space-between">
-        <Box bgColor="white" py="6" shadow="sm">
-          <Container display="flex" alignItems="center" justifyContent="space-between">
-            <Link href="/">
-              <Box cursor="pointer">
-                <IconHome />
+        <Headroom>
+          <Box bgColor="white" py="6" shadow="sm" borderBottomWidth="1px" borderBottomColor="gray.100" >
+            <Container display="flex" alignItems="center" justifyContent="space-between">
+              <Link href="/">
+                <Box cursor="pointer">
+                  <IconHome />
+                </Box>
+              </Link>
+              <Link href="/">
+                <Box w="48" cursor="pointer">
+                  <AutomarketRDLogo />
+                </Box>
+              </Link>
+              <Box>
+                <IconHelp />
               </Box>
-            </Link>
-            <Link href="/">
-              <Box w="48" cursor="pointer">
-                <AutomarketRDLogo />
-              </Box>
-            </Link>
-            <Box>
-              <IconHelp />
-            </Box>
-          </Container>
-        </Box>
+            </Container>
+          </Box>
+        </Headroom>
         <Container my="3">{children}</Container>
         <MainFooter />
       </Box>

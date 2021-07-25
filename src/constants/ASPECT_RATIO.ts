8610d1f1aced74152712df11a5351a6acafed6dd
Erickson Manuel Holguín <38663299+Eriickson@users.noreply.github.com>
2021-07-25 06:44:34 -0400

    Agregando carpeta de constantes

---
+++ src/constants/ASPECT_RATIO.ts
@@ -0,0 +1,7 @@
+import { AspectRatioType } from "@/shared";
+
+export const ASPECT_RATIO: Record<AspectRatioType, number> = {
+  "16:9": 16 / 9,
+  "1:1": 1 / 1,
+  "4:3": 4 / 3,
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export * from "./ASPECT_RATIO";

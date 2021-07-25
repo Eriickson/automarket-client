5600d3891dcc791ded72ba2fd5541f0d326f0f25
Erickson Manuel Holguín <38663299+Eriickson@users.noreply.github.com>
2021-07-25 11:00:41 -0400

    Ajustando diseño sm

--- src/core/layouts/NewLoginLayout.tsx
+++ src/core/layouts/NewLoginLayout.tsx
@@ -1,5 +1,5 @@
-import { AutomarketRDLogo } from "@/assets";
-import { Text, Box, Button } from "@chakra-ui/react";
+import { AutomarketRDIsotipo, AutomarketRDLogo } from "@/assets";
+import { Text, Box, Button, Divider } from "@chakra-ui/react";
 import React, { FC, ReactElement } from "react";
 import { LegalFooter } from "../components/footers/LegalFooter";
 import { AsideLeft } from "../templates/login/signup/home/AsideLeft";
@@ -10,28 +10,31 @@ interface NewLoginLayoutProps {
 
 export const NewLoginLayout: FC<NewLoginLayoutProps> = ({ children }) => {
   return (
-    <Box bg="gray.100" display="flex" flexDir="column" minHeight="100vh">
+    <Box bg={["pri.500", null, "gray.100"]} display="flex" flexDir="column" minHeight="100vh">
       <Box alignItems="stretch" display="flex" flex="1">
         <AsideLeft />
         <Box display="flex" flex={1} flexDirection="column" justifyContent="space-between">
-          <Box
-            alignItems="center"
-            bg="white"
-            display="flex"
-            justifyContent="space-between"
-            px={[3, null, 6, 10]}
-            py={4}
-            shadow="sm"
-            w="full"
-          >
-            <Box w={[56, null, null, 44, 64]}>
-              <AutomarketRDLogo />
+          <Box>
+            <Box
+              alignItems="center"
+              bg="white"
+              display={["none", null, "flex"]}
+              justifyContent="space-between"
+              px={[3, null, 6, 10]}
+              py={4}
+              shadow="sm"
+              w="full"
+            >
+              <Box w={[56, null, null, 44, 64]}>
+                <AutomarketRDLogo />
+              </Box>
+              <Button colorScheme="pri">Inicia Sesión</Button>
             </Box>
-            <Button colorScheme="pri">Inicia Sesión</Button>
           </Box>
           <Box h="auto" maxW={[null, null, null, "max-content"]} pl={[3, null, 6, 10, 8]} pr={[3, null, 6, 0]}>
             <Text
               color="pri.600"
+              display={["none", null, "block"]}
               fontSize={["3xl", null, "4xl", "5xl", "6xl"]}
               fontWeight="black"
               lineHeight={1}
@@ -42,12 +45,21 @@ export const NewLoginLayout: FC<NewLoginLayoutProps> = ({ children }) => {
                 Regístrate para continuar
               </Text>
             </Text>
-            <Box>{children}</Box>
+            <Box bgColor={["white", null, "transparent"]} rounded="md">
+              <Box display={["block", null, "none"]} mx="auto" py="4" w={[12]}>
+                <AutomarketRDIsotipo />
+              </Box>
+              <Divider />
+              <Text fontSize="2xl" fontWeight="bold" mt="5" textAlign="center">
+                Iniciar Sesión
+              </Text>
+              <Box p="5">{children}</Box>
+            </Box>
           </Box>
           <Box h="20"></Box>
         </Box>
       </Box>
-      <Box alignItems="center" bgColor="white" display="flex" h="20">
+      <Box alignItems="center" bgColor="white" display={["none", null, "flex"]} h="20">
         <LegalFooter />
       </Box>
     </Box>

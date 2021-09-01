a83c2ba81cc92fea6bba0198f51fdad0b4516eb3
Erickson Manuel Holguín <erickson01d@gmail.com>
2021-09-01 09:09:29 -0400

    arreglando refreshtoken

--- pages/api/auth/refreshtoken.ts
+++ pages/api/auth/refreshtoken.ts
@@ -1,47 +1,38 @@
 import { NextIronHandler, withSession } from "@/auth";
 import { envs } from "@/config";
-import jwt from "jsonwebtoken";
 import fetch from "isomorphic-unfetch";
-import { AuthPayload } from "@/shared";
 
 const refreshToken: NextIronHandler = async (req, res) => {
   const accessToken = req.session.get("accessToken");
   const refreshUserToken = req.session.get("refreshToken");
 
-  const payload = jwt.decode(accessToken) as AuthPayload;
-
-  // if (accessToken && payload.exp * 1000 > Date.now() / 1000) {
   const data = await fetch(envs.SERVER_GRAPHQL, {
     method: "POST",
+    body: JSON.stringify({
+      query: `
+        mutation RefreshUserToken {
+          refreshUserToken {
+            accessToken
+            refreshToken
+          }
+        }
+      `,
+    }),
     headers: {
       "content-type": "application/json",
       "refresh-user-token": refreshUserToken,
+      authorization: `Bearer ${accessToken}`,
     },
-    body: JSON.stringify({
-      query: `
-          mutation {
-            refreshUserToken(input: {userId: "${payload?.user?.id}"}) {
-              accessToken
-              refreshToken
-            }
-          }
-        `,
-    }),
   }).then(response => response.json());
 
   if (data.errors) {
-    console.log(data.errors);
-
-    console.log("error");
-
     return res.status(401).json({ unauthorized: true });
   }
 
-  req.session.set("accessToken", data.refreshUserToken.accessToken);
-  req.session.set("refreshToken", data.refreshUserToken.refreshToken);
+  req.session.set("accessToken", data.data.refreshUserToken.accessToken);
+  req.session.set("refreshToken", data.data.refreshUserToken.refreshToken);
 
   await req.session.save();
-  // }
   res.json({ successful: true });
 };
 
